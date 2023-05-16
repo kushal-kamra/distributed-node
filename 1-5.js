a716b4b45d5bbe0685a9a131275f4a4e1a4907ee
kushal-kamra <kushalkamradev@gmail.com>
2023-05-16 11:27:15 +0530

    Added event queue priority explanation

--- 1-5.js
+++ 1-5.js
@@ -13,3 +13,12 @@ console.log(8);
 
 // Expected Output
 // 83214765
+
+// Event Loop execution Priority
+// 1 - MicroTask Queue 1 - Callbacks via process.nextTick()
+// 2 - MicroTask Queue 2 - Promises that reject or resolve
+// 3 - Poll - Main application code runs here + I/O related Callbacks
+// 4 - Check - Callbacks via setImmediate();
+// 5 - Close - Callbacks via EventEmitter close events
+// 6 - Timers - Callbacks scheduled via setTimeout() and setInterval()
+// 7 - Pending - Special system events
